07aebbc269ac81cfad9900cf0a4ed7ed856d54fd
Irmak <irmak.ceylan@ug.bilkent.edu.tr>
2025-09-23 21:54:19 +0300

    Update Cipher.java

--- Cipher.java
+++ Cipher.java
@@ -15,7 +15,8 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-
+            char currentChar = inputString.charAt(i);
+            outputString += replaceChar(currentChar, true)
         }
 
         return outputString;
@@ -25,9 +26,11 @@ public class Cipher
         
         // output string will be collected in this variable, one char at a time
         String outputString = "";
-        
-        replaceChar('a',true);
-        
+        for(int i = 0; i < inputString.length(); i++)
+        {
+            char currentChar = inputString.charAt(i);
+            outputString += replaceChar(currentChar, false);
+        }
         return outputString;
     }
 
@@ -42,7 +45,7 @@ public class Cipher
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
         }
@@ -58,4 +61,5 @@ public class Cipher
         // if we did not find it in the alphabet, then return the original char
         return inputChar;
     }
+
 }   
